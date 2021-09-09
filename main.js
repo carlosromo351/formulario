cc2f13eb819890af01750cd440f712597bf0b6f6
carlosromo351 <carlosromo351@gmail.com>
2021-09-09 17:20:36 -0500

    formulario

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
  * 
  * 
 */
+
+
+
 function subir ( ){
 var $Nombres=document.getElementsById('Nombres').value
 var $Apellidos=document.getElementsById('Apellidos').value
@@ -12,8 +15,33 @@ var $numerodedocumento=document.getElementsById('numerodedocumento').value
 var $Movil=document.getElementsById('Movil').value
 var $clave=document.getElementsById('clav').value
 var $VERIFIQUE=document.getElementsById('$VERIFIQUE').value
-const valores= [$Nombres,$Apellidos,$Tipodedocumento,$numerodedocumento,$Movil,$clave]
-alert(valores)
-console.log(valores)
+
+const dataUser=new dataRegister($Nombres,$Apellidos,$Tipodedocumento,$numerodedocumento,$Movil,$clave)
+
+console.log(dataUser)
 
 }
+class dataRegister{
+
+    Nombres
+    Apellidos
+    Tipodedocumento
+    numerodedocumento
+    Movil
+    clave
+    constructor(pnombres,ppellido,
+                pTipodedocumento,
+                Pnumerodedocumento,
+                PMovil,
+                Pclave,){
+
+         this.Nombres= pnombres 
+         this.Apellidos=ppellido
+         this.Tipodedocumento=pTipodedocumento
+         this.numerodedocumento=Pnumerodedocumento
+         this.Movil=PMovil
+         this.clave.Pclave
+    }     
+         
+}
+
