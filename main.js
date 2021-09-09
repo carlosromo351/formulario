8a9c2d0fe702e2caa4d7a8da9364abf8ad7cb5b4
carlosromo351 <carlosromo351@gmail.com>
2021-09-09 10:45:06 -0500

    js

--- main.js
+++ main.js
@@ -4,8 +4,30 @@
  * 
  * 
 */
-var myvariable=3
-console.log(myvariable)
+var $Nombres=document.getElementsById('Nombres').value
+var $Apellidos=document.getElementsById('Apellidos').value
+var $Tipodedocumento=document.getElementsById('Tipodedocumento').value
+var $numerodedocumento=document.getElementsById('numerodedocumento').value
+var $Movil=document.getElementsById('Movil').value
+var $clave=document.getElementsById('clav').value
+var $VERIFIQUE=document.getElementsById('$VERIFIQUE').value
 
-var myvariable="carlos"
-console.warn(myvariable)
+function my_function (a,b) {
+    const suma=a+b
+    console.log(suma )
+    
+}
+ const valor_1 =my_function(4,6)
+ const valor_2=my_function(1100.500)
+
+
+ const funcion =(a,b)=>{
+     const suma=a+b
+     return suma
+ } 
+ const valor_1 =my_function(4,6)   
+ console.log(valor_1 )  
+ const suma=(a,b)=>a+b
+ const resultado =suma(12,5)
+ alert(resultado)
+ 
