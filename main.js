a1b08c859bfb846c1149801001784603fc7f4e61
carlosromo351 <carlosromo351@gmail.com>
2021-09-09 11:07:23 -0500

    js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
  * 
  * 
 */
+function subir (){
 var $Nombres=document.getElementsById('Nombres').value
 var $Apellidos=document.getElementsById('Apellidos').value
 var $Tipodedocumento=document.getElementsById('Tipodedocumento').value
@@ -11,7 +12,9 @@ var $numerodedocumento=document.getElementsById('numerodedocumento').value
 var $Movil=document.getElementsById('Movil').value
 var $clave=document.getElementsById('clav').value
 var $VERIFIQUE=document.getElementsById('$VERIFIQUE').value
-
+const valores= [$Nombres,$Apellidos,$Tipodedocumento,$numerodedocumento,$Movil,$clave]
+alert(valores)
+}
 function my_function (a,b) {
     const suma=a+b
     console.log(suma )
